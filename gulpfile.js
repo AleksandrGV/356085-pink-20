57c6b3c8610d8d23b8d98193e15df0057c184a44
Александр Гончаренко <g.aleks80@gmail.com>
2020-09-09 01:13:55 +0500

    17. Автоматизируем сборку личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
 
 // Styles
 
@@ -16,8 +22,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -49,3 +57,76 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+
+// Imagemin
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+  ]))
+}
+
+exports.images = images;
+
+// Webp
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+ }
+
+ exports.webp = createWebp;
+
+// SVGSprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+  .pipe(svgstore())
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/img/"))
+}
+
+exports.sprite = sprite;
+
+
+//Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+}
+
+exports.copy = copy;
+
+//Clean
+
+const clean = () => {
+  return del("build");
+ };
+
+exports.clean = clean;
+
+//Build
+
+const build = gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
+ );
+
+exports.build = build;
+
